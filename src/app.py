6f18585b7ce6a0c8d45c16398ec97be8a30d0698
Thana Tingprasom <kookza01@gmail.com>
2021-07-31 22:13:30 +0700

    Update

--- src/app.py
+++ src/app.py
@@ -134,3 +134,6 @@ def getTime():
 
 def newFileName(filename, newName):
     return newName + '.' + filename.rsplit('.', 1)[1]
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=3030, debug=False, threaded=True)
